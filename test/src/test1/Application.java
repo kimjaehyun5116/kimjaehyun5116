f0cf7704679589e48d50c279673e158fda633e17
DESKTOP-F4BC13L\hi <ibns789@gmail.com>
2024-03-28 09:31:47 +0900

    test 커밋

--- test/src/test1/Application.java
+++ test/src/test1/Application.java
@@ -1,5 +1,7 @@
 package test1;
 
+import java.util.Random;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -23,5 +25,35 @@ public class Application {
 
         String result = (sum > 0) ? "양수입니다." : "음수입니다.";
         System.out.println("합계가 " + result);
+    }
+    /* TestMethod을 만들고
+    * 1. 인자값을 전달받아 사칙연산과 나머지 계산을 하는
+    * 매개변수를 전달받는 메소드를 만들어 주세요 메소드 이름 Calculator
+    * 출력구문 필요
+    * 2. fianl 키워드를 이용한 실수 값 3.14를 초기화하고
+    * 반지름 값을 입력 받아 실수형태로 값을 리턴 해주는 CircleArea 메소드를 만들어주세요
+    */
+    public static void Calculator(int num1, int num2) {
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("더하기 결과는  : " + sum + "입니다.");
+        System.out.println("빼기 결과는  : " + diff + "입니다.");
+        System.out.println("곱하기 결과는  : " + product + "입니다.");
+        System.out.println("나누기 결과는  : " + quotient + "입니다.");
+        System.out.println("나머지 결과는  : " + remainder + "입니다.");
+    }
+    public static double circleArea(final double radius){
+        final double PI = Math.PI;
+        double area = PI * radius * radius;
+        return area;
+    }
+    public static String Random(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(10)+1;
+        return "생선된 난수는 " + randomNumber + "입니다.";
     }
 }
--- test/src/test2/Application.java
+++ test/src/test2/Application.java
@@ -1,18 +1,14 @@
 package test2;
 
-import test1.TestMethod;
-
 public class Application {
 
     public static void main(String[] args) {
 
-        TestMethod.calculator(2, 5);
-
+        test1.Application.Calculator(2, 5);
 
         double radius = 5.0;
-        System.out.println("원의 넓이는 " + TestMethod.circleArea(radius) + "입니다.");
+        System.out.println("원의 넓이는 " + test1.Application.circleArea(radius) + "입니다.");
 
-
-        System.out.println(TestMethod.testRandom());
+        System.out.println(test1.Application.Random());
     }
 }
