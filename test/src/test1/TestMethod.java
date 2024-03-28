339e0acd27bc554288ce401ec6eb500f54e62e99
DESKTOP-F4BC13L\hi <ibns789@gmail.com>
2024-03-28 09:08:26 +0900

    Revert "test 컴잇"
    This reverts commit 259fd4a333bc471897e2b5a87aef95b1037740f9.

---
+++ test/src/test1/TestMethod.java
@@ -0,0 +1,47 @@
+package test1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TestMethod {
+
+    public static void calculator(int num1, int num2) {
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("덧셈 결과: " + sum);
+        System.out.println("뺄셈 결과: " + diff);
+        System.out.println("곱셈 결과: " + product);
+        System.out.println("나눗셈 결과: " + quotient);
+        System.out.println("나머지 결과: " + remainder);
+    }
+
+    public static double circleArea(final double radius) {
+        final double PI = 3.14;
+        double area = PI * radius * radius;
+        return area;
+    }
+
+    public static String testRandom() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(10) + 1; // 1부터 10까지의 난수 생성
+        return "생성된 난수는 " + randomNumber + "입니다.";
+    }
+
+    public static void main(String[] args) {
+
+        calculator(2, 5);
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("반지름을 입력하세요: ");
+        double radius = scanner.nextDouble();
+        System.out.println("원의 넓이는 " + radius + "입니다.");
+
+
+        System.out.println(testRandom());
+    }
+}
--- test/src/test2/Application.java
+++ test/src/test2/Application.java
@@ -1,16 +1,18 @@
 package test2;
+
+import test1.TestMethod;
+
 public class Application {
-    /*1. TestMethod의 Calculator을 전달인자로 2,5를 주어 값을 출력해주세요
-    * 2. TestMethod의 CircleArea를 원의 넓이는 **입니다를 출력하는 구문을 만들어주세요
-    * 3. TestMethod의 TestRandome을 출력해주세요
-     */
+
     public static void main(String[] args) {
 
-        Application testMethod = new Application();
+        TestMethod.calculator(2, 5);
 
-        test1.Application.calculator(2, 5);
+
         double radius = 5.0;
-       System.out.println("원의 넓이는 " + test1.Application.circleArea(radius) + "입니다.");
-       System.out.println(test1.Application.Random());
+        System.out.println("원의 넓이는 " + TestMethod.circleArea(radius) + "입니다.");
+
+
+        System.out.println(TestMethod.testRandom());
     }
 }
