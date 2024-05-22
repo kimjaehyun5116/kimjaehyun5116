f6fa3dcf92ee5efbfdf4eb1352862714531aecf0
김재현 <>
2024-05-22 13:48:13 +0900

    커밋

---
+++ FastCampus/src/IfElseIfTest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class IfElseIfTest {
+    public static void main(String[] args) {
+        // Q. 80점 이상의 학생 중에서 90점 이상은 A반, 85점 이상은 B반,
+        // 그렇지 않으면 C반으로 편성하고 점수가 80 미만이면 불합격 처리하는 프로그램을 작성하세요
+        System.out.print("점수를 입력하세요 : ");
+        Scanner sc=new Scanner(System.in);
+        int score=sc.nextInt();
+        if(score>=0 && score<=100){
+             if(score>=90){
+                  System.out.println("A반");
+              }else if(score>=85){
+                  System.out.println("B반");
+             }else if(score>=80){
+                   System.out.println("C반");
+             }else if(score<80) {
+                 System.out.println("불합격");
+             }
+        }else{
+            System.out.println("유효한 점수가 아닙니다");
+        }
+    }
+}
--- FastCampus/src/IfElseTest.java
+++ FastCampus/src/IfElseTest.java
@@ -25,5 +25,13 @@ public class IfElseTest {
         }else{
             System.out.println("12의 배수가 아닙니다");
         }
+        // Q. 년도를 입력 받아서 해당 년도가 윤년인지 아닌지를 판단하여 출력하는 코드를 작성하세요
+        System.out.print("년도를 입력하세요 : ");
+        int year=sc.nextInt();
+        if((year%4==0 && year%100!=0) || (year%400==0)){
+            System.out.println("윤년 입니다");
+        }else{
+            System.out.println("윤년이 아닙니다");
+        }
     }
 }
