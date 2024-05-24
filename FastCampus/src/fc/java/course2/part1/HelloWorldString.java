698af6564cbea4d92e8d1fd9102fa4f7b0a6743e
김재현 <>
2024-05-24 21:18:16 +0900

    커밋

---
+++ FastCampus/src/fc/java/course2/part1/HelloWorldString.java
@@ -0,0 +1,11 @@
+package fc.java.course2.part1;
+// import java.lang.*;
+public class HelloWorldString {
+    public static void main(String[] args) {
+        String str=new String("HelloWorld");
+        System.out.println(str.toString());
+
+        String str1="HelloWorld";
+        System.out.println(str1.toString());
+    }
+}
--- FastCampus/src/fc/java/model2/ObjectArray.java
+++ FastCampus/src/fc/java/model2/ObjectArray.java
@@ -2,15 +2,20 @@ package fc.java.model2;
 
 import java.util.Arrays;
 
-public class BookArray {
+public class ObjectArray {
     private final int DEFAULT_CAPACITY=5; // 수정불가(final)=>상수
-    private Book[] elements;
+    private Object[] elements; // 다형성배열
     private int size=0;
-    public BookArray() {
-        elements=new Book[DEFAULT_CAPACITY]; // 5개 크기 배열
+    public ObjectArray() {
+
+        // elements=new Object[DEFAULT_CAPACITY]; // 5개 크기 배열
+        this(5); // this() : 생성자 안에서 다른 생성자를 호출할때 사용
+    }
+    public ObjectArray(int capacity) {
+        elements=new Object[capacity]; // 원하는 크기의 배열을 생성한다
     }
     // 저장하는 동작
-    public void add(Book element){
+    public void add(Object element){
         if(size==elements.length) {
             // 크기를 2배로 늘리는 작업
             ensureCapacity();
@@ -19,7 +24,7 @@ public class BookArray {
 
     }
     // 얻는 동작
-    public Book get(int index){ // -1, 5~
+    public Object get(int index){ // -1, 5~
         if(index<0 || index>=size) {
             throw new IndexOutOfBoundsException("index 의 범위 (0~9)가 초과됨"); //JVM
         }
