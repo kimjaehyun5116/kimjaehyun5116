0b3780b8253f5532a298266ef9e040d7fb167bdb
Ethan Kim(김재현) <ibns789@gmail.com>
2024-06-12 22:36:11 +0900

    6월 11일 배달웹

---
+++ springboot-part01/13. delivery-service/service/api/src/main/java/org/delivery/api/account/model/AccountMeResponse.java
@@ -0,0 +1,17 @@
+package org.delivery.api.account.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class AccountMeResponse {
+
+    private String email;
+    private String name;
+    private LocalDateTime registeredAt;
+}
